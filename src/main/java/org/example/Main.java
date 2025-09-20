8c0dd09d25967d21d62cda0468ad9cbd8e83ff82
dk872 <dimakulyk2005@gmail.com>
2025-09-20 17:33:51 +0300

    Add Main class demonstrating Clothing usage

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Main {
+
+    /**
+     * Demonstrates usage of the Clothing class:
+     * - creating an array of objects
+     * - sorting by different fields
+     * - searching for an identical object
+     */
+    public static void main(String[] args) {
+        // Create an array of Clothing objects
+        Clothing[] clothes = {
+                new Clothing("T-shirt", "Nike", "Red", 25.0, 42),
+                new Clothing("Jacket", "Adidas", "Black", 120.0, 44),
+                new Clothing("Jeans", "Levi's", "Blue", 80.0, 40),
+                new Clothing("Sweater", "Puma", "Green", 60.0, 43),
+                new Clothing("Shirt", "H&M", "White", 30.0, 41)
+        };
+
+        System.out.println("=== Initial array ===");
+        Arrays.stream(clothes).forEach(System.out::println);
+    }
+}
